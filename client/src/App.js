3c4ce03f8462eaab80708aaad7d561f0a57660cb
Alex <alexander_sck@live.ca>
2022-11-03 12:03:36 -0400

    fix: fix file structure and remove venv from git

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Menu from './components/Menu';
 import ForcastGraph from './components/ForecastGraph';
 import PerformanceGraph from './components/PerformanceGraph';
+import {ReactComponent as Stocks} from './assets/stock-increase.svg';
 
 function App() {
 
@@ -13,10 +14,11 @@ function App() {
         <div className='app-container'>
             <div className='header'> 
                 <h1>Stock Forecast</h1>
+                <Stocks />
             </div>
 
             <div className='main'>
-
+                
             </div>
 
             <div className='footer'>
