30642dd8640d0629aae84bd57e9e9e99a71870e9
Alex <alexander_sck@live.ca>
2023-01-09 18:51:36 -0500

    refactor: break endpoints down to target sequential or functional models

--- app.py
+++ app.py
@@ -1,53 +1,48 @@
 from flask import Flask, request
-from analyser import stock_analysis
+from distributer import distributer
 
 app = Flask(__name__)
 
-@app.route('/api/get_stock_prediction', methods=['GET'])
-def get_stock_prediction():
+@app.route('/api/single_day_forecast', methods=['GET'])
+def sequential_forecast():
     """
-    Project a given stock performance over a period of time.
+    Forecast stock performance over a single day.
 
     Parameters
     ----------
     ticker : string
         Ticker of stock to be projected.
-    period : int
-        Time period of stock projection.
-    model : string
-        Specified compilation model.
 
     Returns
     -------
-    current_data : list
-        Array of current stock performance.
-    forecasted_data : list
-        Array of projected stock performance.
+    status : string
+        Status of request.
+    forecasted_data : int
+        Projected stock price.
+    rate : int
+        Performance rating compared to previous date.
 
     Example
     -------
-    Project Apple, Inc. stock performance over the next 3 months.
+    Project Apple, Inc. stock performance tomorrow.
 
-    Send `GET` request to ".../api/get_stock_prediction"
-        include `ticker` and `period` in json parameters.
+    Send `GET` request to ".../api/single_day_forecast"
         >>> request = {
                 "ticker": "AAPL",
-                "period": 3,
-                "model": "optimized"
             }
 
-    Parse `current_data` and `forecasted_data` returned in json object.
-        >>> response["current_data"]
-        >>> response["forecasted_data"]
+        >>> response = {
+                "status": "ok",
+                "forecasted_data": $VALUE$,
+                "rate": %RATE%
+            }
     """
 
     try:
         req = request.json
         ticker = req['ticker']
-        period = req['period']
-        model = req['model']
 
-        current_data, forecasted_data = stock_analysis(ticker, period, model)
+        forecasted_data, rate = distributer(ticker, period=1, model='sequential')
     except:
         return {
             'status': 'error',
@@ -56,10 +51,65 @@ def get_stock_prediction():
 
     return {
         'status': 'ok',
-        'current_data': [
-            current_data
-        ],
-        'forecasted_data': [
-            forecasted_data
-        ],
+        'forecasted_data': forecasted_data,
+        'rate': rate,
+    }
+
+@app.route('/api/multi_day_forecast', methods=['GET'])
+def functional_forecast():
+    """
+    Forecast stock performance over multiple days.
+
+    Parameters
+    ----------
+    ticker : string
+        Ticker of stock to be projected.
+    period : int
+        Number of days to forecast up to (inclusive).
+
+    Returns
+    -------
+    status : string
+        Status of request.
+    forecasted_data : list
+        Array of projected stock performance over given period.
+    rate : int
+        Performance rating compared to initial date.
+
+    Example
+    -------
+    Project Apple, Inc. stock performance over the next 30 days.
+
+    Send `GET` request to ".../api/get_stock_prediction"
+        >>> request = {
+                "ticker": "AAPL",
+                "period": 30
+            }
+
+        >>> response = {
+                "status": "ok",
+                "forecasted_data": {
+                    "DATE": $VALUE$,
+                    ...
+                },
+                "rate": %RATE%
+            }
+    """
+
+    try:
+        req = request.json
+        ticker = req['ticker']
+        period = req['period']
+
+        forecasted_data, rate = distributer(ticker, period, model='functional')
+    except:
+        return {
+            'status': 'error',
+            'error': ''
+        }
+
+    return {
+        'status': 'ok',
+        'forecasted_data': forecasted_data,
+        'rate': rate,
     }
