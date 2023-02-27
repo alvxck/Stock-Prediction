bac4a53a8c8a6fc3ad692ae74b0fc3363fe43dea
Alex Carvalho <alexander_sck@live.ca>
2023-02-27 12:14:58 -0500

    refactor: update training params for sequential model

--- training/sequential.py
+++ training/sequential.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import yfinance as yf
 import datetime as dt 
+import matplotlib.pyplot as plt
 
 from pandas_datareader import data as pdr
 from sklearn.preprocessing import MinMaxScaler
@@ -38,40 +39,42 @@ def sequential(ticker):
     yf.pdr_override()
     
     # Load Data
-    start = dt.datetime(2000, 1, 1)
+    start = dt.datetime.now() - dt.timedelta(days=5475)
     end = dt.datetime.now()
 
     data = pdr.get_data_yahoo(ticker, start, end)
 
-    # Pre-process Data
-    scaler = MinMaxScaler(feature_range=(0,1))
-    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+    # Pre-process data
+    close_data = data['Close'].values.reshape(-1, 1)
+    train_len = int(np.ceil(len(close_data) * .95))
 
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(close_data)
+
+    # Train model on accuired data
     prediction_days = 60
+    train_data = scaled_data[0:int(train_len), :]
 
     x_train = []
     y_train = []
 
-    for x in range(prediction_days, len(scaled_data)):
-        x_train.append(scaled_data[x-prediction_days:x, 0])
-        y_train.append(scaled_data[x, 0])
+    for i in range(prediction_days, len(train_data)):
+        x_train.append(train_data[i-prediction_days: i, 0])
+        y_train.append(train_data[i, 0])
 
     x_train, y_train = np.array(x_train), np.array(y_train)
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
     # Build Model
     model = Sequential()
-
-    model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-    model.add(Dropout(0.2))
-    model.add(LSTM(units=50, return_sequences=True))
-    model.add(Dropout(0.2))
-    model.add(LSTM(units=50))
-    model.add(Dropout(0.2))
-    model.add(Dense(units=1))
+    model.add(LSTM(units=64, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+    model.add(LSTM(units=64))
+    model.add(Dense(32))
+    model.add(Dropout(.5))
+    model.add(Dense(1))
 
     model.compile(optimizer='adam', loss='mean_squared_error')
-    model.fit(x_train, y_train, epochs=25, batch_size=32)
+    model.fit(x_train, y_train, epochs=10)
 
     model.save(f'models/{ticker}-seq')
 
